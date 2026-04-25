98034b5294068cf0ad434e3ce2fe4469b5051a23
Dorukkozkn <77771219+Dorukkozkn@users.noreply.github.com>
2026-04-25 10:40:00 +0300

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -86,7 +86,7 @@ Para Birimi: ${currency}`
         Lüks Ulaşımın <span className="text-gold">Yeni Adresi</span>
       </h1>
 
-      <p className="text-gold text-sm md:text-base tracking-[0.6em] uppercase mb-8">
+      <p className="text-white text-sm md:text-base tracking-[0.5em] uppercase mb-8">
         SZKVIPTRANSFER
       </p>
 
